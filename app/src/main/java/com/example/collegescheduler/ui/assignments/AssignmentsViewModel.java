55444287ab5eaf228d1e1b8ad3518b247c7efdae
Hebe3834 <hebeluna00@gmail.com>
2024-01-27 20:26:58 -0500

    refactored gallery into assignments

--- app/src/main/java/com/example/collegescheduler/ui/assignments/AssignmentsViewModel.java
+++ app/src/main/java/com/example/collegescheduler/ui/assignments/AssignmentsViewModel.java
@@ -1,16 +1,16 @@
-package com.example.collegescheduler.ui.gallery;
+package com.example.collegescheduler.ui.assignments;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class AssignmentsViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public AssignmentsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mText.setValue("This is assignments fragment");
     }
 
     public LiveData<String> getText() {
