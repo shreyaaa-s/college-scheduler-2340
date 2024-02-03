53c919350716dad50eb174e43c4e856d90c039b4
shreyaaa-s <ssasmal7@gatech.edu>
2024-02-03 17:28:07 -0500

    added assignments page functionality

--- app/src/main/java/com/example/collegescheduler/ui/assignments/AssignmentsFragment.java
+++ app/src/main/java/com/example/collegescheduler/ui/assignments/AssignmentsFragment.java
@@ -1,32 +1,91 @@
 package com.example.collegescheduler.ui.assignments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.collegescheduler.R;
 import com.example.collegescheduler.databinding.FragmentAssignmentsBinding;
+
+import java.util.ArrayList;
 
 public class AssignmentsFragment extends Fragment {
 
     private FragmentAssignmentsBinding binding;
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView listView;
+    private Button button;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        AssignmentsViewModel assignmentsViewModel =
-                new ViewModelProvider(this).get(AssignmentsViewModel.class);
-
+        super.onCreate(savedInstanceState);
+//        setContentView(binding.getRoot());
         binding = FragmentAssignmentsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textAssignments;
-        assignmentsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        listView = root.findViewById(R.id.listView);
+        button = root.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addItem(view, root);
+            }
+        });
+
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(root.getContext(), android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(itemsAdapter);
+        setUpListViewListener();
+
+//        AssignmentsViewModel assignmentsViewModel =
+//                new ViewModelProvider(this).get(AssignmentsViewModel.class);
+//
+//        binding = FragmentAssignmentsBinding.inflate(inflater, container, false);
+//        View root = binding.getRoot();
+//
+//        final TextView textView = binding.textAssignments;
+//        assignmentsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+
+    private void setUpListViewListener() {
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Context context = getActivity().getApplicationContext();
+                Toast.makeText(context, "Assignment Removed", Toast.LENGTH_LONG).show();
+
+                items.remove(position);
+                itemsAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+    }
+
+    private void addItem(View view, View root) {
+        EditText input = root.findViewById(R.id.editTextText);
+        String itemText = input.getText().toString();
+
+        if(!(itemText.equals(""))){
+            itemsAdapter.add(itemText);
+        } else{
+            Toast.makeText(getActivity().getApplicationContext(), "Please enter text...", Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
