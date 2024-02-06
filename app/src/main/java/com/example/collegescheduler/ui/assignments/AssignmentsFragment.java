64b2e8fad36341f57b96788c2c8d0d9b10f97bc5
clifflin33 <clifflin33@gmail.com>
2024-02-06 15:30:29 -0500

    SORTING!!! and Buttons

--- app/src/main/java/com/example/collegescheduler/ui/assignments/AssignmentsFragment.java
+++ app/src/main/java/com/example/collegescheduler/ui/assignments/AssignmentsFragment.java
@@ -41,7 +41,8 @@ public class AssignmentsFragment extends Fragment {
     private DatePickerDialog datePickerDialogue;
 //    private Button dateButton;
     private Button datePickerButton;
-    private Button sortButton;
+    private Button sortButtonDate;
+    private Button sortButtonClass;
 //    private Calendar calendar;
     private TextView editDateText;
 
@@ -57,7 +58,8 @@ public class AssignmentsFragment extends Fragment {
         button = root.findViewById(R.id.button);
         datePickerButton = root.findViewById(R.id.datePickerButton);
         editDateText = root.findViewById(R.id.editDateText);
-        sortButton = root.findViewById(R.id.sortButton);
+        sortButtonDate = root.findViewById(R.id.sortButtonDate);
+        sortButtonClass = root.findViewById(R.id.sortButtonClass);
 
         assignmentsViewModel = new ViewModelProvider(this).get(AssignmentsViewModel.class);
 
@@ -72,10 +74,18 @@ public class AssignmentsFragment extends Fragment {
 
         //Sorting changes
 
-        sortButton.setOnClickListener(new View.OnClickListener() {
+        sortButtonDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 assignmentsViewModel.sortArray(1);
+                itemsAdapter.notifyDataSetChanged();
+            }
+        });
+
+        sortButtonClass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                assignmentsViewModel.sortArray(2);
                 itemsAdapter.notifyDataSetChanged();
             }
         });
